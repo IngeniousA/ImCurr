002e4b89e5b24baf13599cf150610cc1322f3ed3
Sergey Rakhmanov <rpden@yandex.ru>
2017-08-04 11:23:10 +0300

    0.4.1.2 final version
    Minor fixes.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 #define _CRT_SECURE_NO_WARNINGS
-#define _D_SCL_SECURE_NO_WARNINGS
 #define _ITERATOR_DEBUG_LEVEL 0 
 
 #include <iostream>
@@ -13,26 +12,28 @@
 #include <ctime>
 #include <conio.h>
 #include <stdio.h>
+#include <io.h>
 #include "sha256.h"
-//#include "msglib.h"
 
 #define SMX 16
 #define MX 256
 #define MMX 65535
 #define LMX 1500000
 #define FIRST_SYM 33
-#define HIDELIM 89
-#define SAFE_BORDER 20
+#define HIDELIM 255
+#define SAFE_BORDER 256
 #define HASH 65
 
-char inputch[MX];
 using namespace std;
+
+char inputch[MX];
+int qIndex = 0;
 
 struct BasicKey
 {
 	int size = 64;
 	char fwd = 0;
-	const int startpos = 65;
+	const int startpos = size + 1;
 	string raw;
 	string toCheck;
 	string hash;
@@ -41,26 +42,29 @@ struct BasicKey
 struct Segment
 {
 	int toEdit;
-	int size;
+	long int size;
 	int buffer;
 	int endpos;
 	int segments;
 };
 
-struct Trloc
+struct Translocation
 {
 	int limit = MMX;
 	string tmpA;
 	string tmpB;
 };
 
-class Image
+class BasicFile
 {
 public:
 	void setp(char * path); //set path
-	void scsn(); //Subconsciuousness menu
+	void additional();
+	void scsn();
 	void scsnE(); //SCSN - encryption
 	void scsnD(); //SCSN - decryption
+	void hide(); //hide a message
+	void get(); //get a message
 
 private:
 	char fullpath[MX]; //Input path
@@ -70,12 +74,16 @@ private:
 	ofstream imgO; 
 };
 
-Image img;
+vector<BasicFile> ImQ;
 
-void Image::scsnD() //decryption
+void BasicFile::scsnD() //decryption
 {
 	BasicKey key; //initialization for key, shuffling strings and segment parameters
-	Trloc trs;
+	key.raw.clear();
+	key.raw.resize(MX);
+	key.toCheck.clear();
+	key.toCheck.resize(MX);
+	Translocation trs;
 	Segment segm;
 	bool amode = false; //automatic mode = false
 	int segment = 0; //segment size
@@ -111,9 +119,13 @@ void Image::scsnD() //decryption
 			key.fwd++; //counting char shifting
 		}
 	}
-	imgI.open(fullpath, ios::binary); //getting hash
-	imgI >> key.hash;
-	key.hash.resize(64);
+	key.toCheck.resize(it);
+	key.hash.resize(64);	
+	imgI.open(fullpath, ios::in); //getting hash
+	for (size_t i = 0; i < 64; i++)
+	{
+		imgI.get(key.hash[i]);
+	}
 	imgI.close();
 	if (sha256(key.toCheck) == key.hash) //if password is correct
 	{		
@@ -147,13 +159,13 @@ void Image::scsnD() //decryption
 		ofstream  dst(fullpathN, ios::binary);
 
 		char c; //geting chars which were shuffled
-		vector<char>toE2;
-		toE2.resize(segm.toEdit);
+		vector<char>toShuffle;
+		toShuffle.resize(segm.toEdit);
 		src.seekg(pointer, ios::beg);
 		for (int i = 0; i < segm.toEdit; i++)
 		{
 			src.get(c);
-			toE2[i] = c;
+			toShuffle[i] = c;
 		}
 		
 		trs.tmpA.resize(segment);
@@ -165,13 +177,13 @@ void Image::scsnD() //decryption
 		{
 			for (int i = 0; i < segment; i++)
 			{
-				trs.tmpA[i] = (char)(toE2[used + i] - key.fwd);
+				trs.tmpA[i] = (char)(toShuffle[used + i] - key.fwd);
 			}
 			uSegments++;
 			used += segment;
 			for (int i = 0; i < segment; i++)
 			{
-				trs.tmpB[i] = (char)(toE2[used + i] - key.fwd);
+				trs.tmpB[i] = (char)(toShuffle[used + i] - key.fwd);
 			}
 			uSegments++;
 			used += segment;
@@ -192,7 +204,7 @@ void Image::scsnD() //decryption
 	}
 	else //incorrect password
 	{
-		cout << "Incorrect key!" << endl;
+		cout << "\nIncorrect key!" << endl;
 		cout << "SHA-256 of entered string: \n/" + sha256(key.toCheck) + "/\n";
 		cout << "SHA-256 of key: \n/" + key.hash + "/\n";
 		imgI.close();
@@ -202,11 +214,63 @@ void Image::scsnD() //decryption
 	exit(0);
 }
 
-void Image::scsnE() //encryption
+void BasicFile::hide()
+{
+	char num = '!';
+	char msg[MMX] = { 0 };
+	system("cls");
+	imgW.open(fullpath, ios::app);
+	cout << "Message: ";
+	while (strlen(msg) == 0)
+	{
+		cin.getline(msg, MMX);
+	}
+	num += strlen(msg);
+	imgW << msg << num;
+	imgW.close();
+}
+
+void BasicFile::get()
+{
+	bool st = true;
+	char msg[MMX] = { 0 };
+	char getn = '\0';
+	memset(msg, 0, sizeof(msg));
+	system("cls");
+	cout << "Message ressurection mode" << endl;
+	imgW.open(fullpath);
+	imgW.seekg(-1, imgW.end);
+	imgW >> getn;
+	if ((getn > FIRST_SYM) && (getn < (FIRST_SYM + HIDELIM))) {
+		int delta = abs(getn - (FIRST_SYM - 1));
+		imgW.seekg(-delta, imgW.cur);
+		imgW.getline(msg, getn - FIRST_SYM + 1);
+		if (strlen(msg) > 0)
+		{
+			cout << "Hidden message: " << msg << endl;
+			system("pause");
+		}
+		else 
+		{
+			cout << "Failed to find a message, probably the file doesn't exist \n or the name given is wrong." << endl;
+			system("pause");
+		}
+	}
+	else
+	{
+		cout << "Failed to find a message, probably the file doesn't exist \n or the name given is wrong." << endl;
+		system("pause");
+	}
+	imgW.close();
+	scsn();
+}
+
+void BasicFile::scsnE() //encryption
 {
 	BasicKey key; //initialization for key, shuffling strings and segment parameters
 	key.raw.clear();
-	Trloc trs;
+	key.raw.resize(MX);
+	Translocation trs;
 	Segment segm;
 	int segment = 0; //setting default values
 	int pointer = 0;
@@ -248,6 +312,7 @@ void Image::scsnE() //encryption
 			key.fwd++;
 		}
 	}	
+	key.raw.resize(it);
 	key.hash = sha256(key.raw);
 	if (segm.size <= 5000) //automatic is now default
 	{
@@ -270,12 +335,12 @@ void Image::scsnE() //encryption
 	ofstream  dst(fullpathN, ios::binary); 
 
 	char c;
-	vector<char>toE2;
-	toE2.resize(segm.toEdit);
+	vector<char>toShuffle;
+	toShuffle.resize(segm.toEdit);
 	for (int i = 0; i < segm.toEdit; i++)
 	{
 		src.get(c);
-		toE2[i] = c;
+		toShuffle[i] = c;
 	}
 
 	dst << key.hash << '\0'; //send hash and '\0' char to divide the key part from the file part
@@ -288,13 +353,13 @@ void Image::scsnE() //encryption
 	{
 		for (int i = 0; i < segment; i++)
 		{
-			trs.tmpA[i] = (char)(toE2[used + i] + key.fwd);
+			trs.tmpA[i] = (char)(toShuffle[used + i] + key.fwd);
 		}
 		uSegments++;
 		used += segment;
 		for (int i = 0; i < segment; i++)
 		{
-			trs.tmpB[i] = (char)(toE2[used + i] + key.fwd); 
+			trs.tmpB[i] = (char)(toShuffle[used + i] + key.fwd); 
 		}
 		uSegments++;
 		used += segment;
@@ -312,12 +377,13 @@ void Image::scsnE() //encryption
 	dst.close();
 }
 
-void Image::scsn() //menu
+void BasicFile::scsn() //menu
 {
 	system("cls");
-	cout << "1 - Encrypt a file" << endl 
-		 << "2 - Decrypt a file" << endl 
-		 << "3 - Exit" << endl;
+	cout << "1 - Encrypt a file" << endl
+		<< "2 - Decrypt a file" << endl
+		<< "3 - Other" << endl
+		<< "4 - Exit" << endl;
 	char ch;
 	cin >> ch;
 	cin.clear();
@@ -329,7 +395,10 @@ void Image::scsn() //menu
 		scsnD();
 		break;
 	case '3':
+		additional();
 		exit(0);
+		break;
+	case '4':
 		break;
 	default:
 		cout << "Incorrect input!" << endl;
@@ -338,38 +407,88 @@ void Image::scsn() //menu
 	}
 }
 
-void Image::setp(char * path) //set path to file
+inline bool fileOK(const char * name) 
 {
-	strcpy(fullpath, path);
+	struct stat buffer;
+	return (stat(name, &buffer) == 0);
+}
+
+void BasicFile::setp(char * path) //set path to file
+{
+	if (fileOK(path))
+	{
+		strcpy(fullpath, path);
+	}
+	else
+	{
+		cout << "Incorrect path!\n";
+		system("pause");
+		exit(0);
+		return;
+	}
+}
+
+void BasicFile::additional()
+{
+	system("cls");
+	cout << "1 - Hide a message" << endl
+		<< "2 - Get a message" << endl
+		<< "3 - Back" << endl;
+	char ch;
+	cin >> ch;
+	cin.clear();
+	switch (ch) {
+	case '1':
+		hide();
+		break;
+	case '2':
+		get();
+		break;
+	case '3':
+		scsn();
+	default:
+		cout << "Incorrect input!" << endl;
+		additional();
+		break;
+	}
 }
 
 int main()
 {
 	memset(inputch, 0, sizeof(inputch));
 	short int ch = 0;
+	int qSize = 0;
 	cout << "Name (1 to exit) \n>";
 	while (strlen(inputch) == 0)
 	{
-		cin.getline(inputch, LMX);
+		cin.getline(inputch, LMX);		
 	}
 	if ((inputch[0] == '1') && (strlen(inputch) == 1))
 	{
 		exit(0);
 	}
+	else if ((inputch[0] == 'Q') && (strlen(inputch) == 1))
+	{
+		cout << "Enter the size of queue: ";
+		cin >> qSize;
+		ImQ.resize(qSize);
+	}
 	else
 	{
-		img.setp(inputch);
+		qIndex = 0;
+		ImQ.resize(1);
+		ImQ[0].setp(inputch);
+		ImQ[0].scsn();
 	}
-	img.scsn();
 	return 0;
 }
 
 /*
-	ImCurr 0.4 source code.
+	ImCurr 0.4.1.1 source code.
 	Made by Sergey 'Ingenious' Rakhmanov, for free non-profit use.
 	If you want to contact me, there are my credits:
 
 	GitHub: IngeniousA
 	VK: vk.com/1ngenious
-	E-Mail: crashtranslator@yandex.ru
+	E-Mail: IngeniousA@yandex.ru
 */
